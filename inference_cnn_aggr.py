669a386fc85a973db9f41e753516957dc0751a6f
hyunho.mo <hyunho.mo@unitn.it>
2021-08-15 02:34:23 +0200

    check

--- inference_cnn_aggr.py
+++ inference_cnn_aggr.py
@@ -133,9 +133,11 @@ def rmse(y_true, y_pred):
 
 def shuffle_array(sample_array, label_array):
     ind_list = list(range(len(sample_array)))
-    print("ind_list befor: ", ind_list)
+    print("ind_list befor: ", ind_list[:10])
+    print("ind_list befor: ", ind_list[-10:])
     shuffle(ind_list)
-    print("ind_list after: ", ind_list)
+    print("ind_list after: ", ind_list[:10])
+    print("ind_list after: ", ind_list[-10:])
     shuffle_sample = sample_array[ind_list, :, :]
     shuffle_label = label_array[ind_list,]
     return shuffle_sample, shuffle_label
