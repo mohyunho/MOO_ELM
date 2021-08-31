5e9e806884747e36248eef46a73cdc5533d6f9ba
hyunho.mo <hyunho.mo@unitn.it>
2021-08-31 14:34:43 +0200

    oselm

---
+++ inference_oselm.py
@@ -0,0 +1,412 @@
+'''
+DL models (FNN, 1D CNN and CNN-LSTM) evaluation on N-CMAPSS
+12.07.2021
+Hyunho Mo
+hyunho.mo@unitn.it
+'''
+## Import libraries in python
+import gc
+import argparse
+import os
+import json
+import logging
+import sys
+import h5py
+import time
+import matplotlib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import math
+import random
+from random import shuffle
+from tqdm.keras import TqdmCallback
+
+seed = 0
+random.seed(0)
+np.random.seed(seed)
+
+import importlib
+from scipy.stats import randint, expon, uniform
+import sklearn as sk
+from sklearn import svm
+from sklearn.utils import shuffle
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import pipeline
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from tqdm import tqdm
+import scipy.stats as stats
+# from sklearn.utils.testing import ignore_warnings
+# from sklearn.exceptions import ConvergenceWarning
+# import keras
+import tensorflow as tf
+print(tf.__version__)
+# import keras.backend as K
+import tensorflow.keras.backend as K
+from tensorflow.keras import backend
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.layers import Input, Dense, Flatten, Dropout, Embedding
+from tensorflow.keras.layers import BatchNormalization, Activation, LSTM, TimeDistributed, Bidirectional
+from tensorflow.keras.layers import Conv1D
+from tensorflow.keras.layers import MaxPooling1D
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
+
+
+from tensorflow.python.framework.convert_to_constants import  convert_variables_to_constants_v2_as_graph
+
+from tensorflow.keras.initializers import GlorotNormal, GlorotUniform
+
+initializer = GlorotNormal(seed=0)
+# initializer = GlorotUniform(seed=0)
+
+from utils.data_preparation_unit import df_all_creator, df_train_creator, df_test_creator, Input_Gen
+# from utils.dnn import one_dcnn
+from utils.os_elm import OS_ELM
+
+
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
+
+# Ignore tf err log
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
+# config = ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = InteractiveSession(config=config)
+
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#for gpu in gpus:
+#    tf.config.experimental.set_memory_growth(gpu, True)
+
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# tf.get_logger().setLevel(logging.ERROR)
+
+
+
+# tf.config.set_visible_devices([], 'GPU')
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_filedir = os.path.join(current_dir, 'N-CMAPSS')
+data_filepath = os.path.join(current_dir, 'N-CMAPSS', 'N-CMAPSS_DS02-006.h5')
+sample_dir_path = os.path.join(data_filedir, 'Samples_whole')
+
+model_temp_path = os.path.join(current_dir, 'Models', 'oned_cnn_rep.h5')
+tf_temp_path = os.path.join(current_dir, 'TF_Model_tf')
+
+pic_dir = os.path.join(current_dir, 'Figures')
+
+'''
+load array from npz files
+'''
+def load_part_array (sample_dir_path, unit_num, win_len, stride, part_num):
+    filename =  'Unit%s_win%s_str%s_part%s.npz' %(str(int(unit_num)), win_len, stride, part_num)
+    filepath =  os.path.join(sample_dir_path, filename)
+    loaded = np.load(filepath)
+    return loaded['sample'], loaded['label']
+
+def load_part_array_merge (sample_dir_path, unit_num, win_len, win_stride, partition):
+    sample_array_lst = []
+    label_array_lst = []
+    print ("Unit: ", unit_num)
+    for part in range(partition):
+      print ("Part.", part+1)
+      sample_array, label_array = load_part_array (sample_dir_path, unit_num, win_len, win_stride, part+1)
+      sample_array_lst.append(sample_array)
+      label_array_lst.append(label_array)
+    sample_array = np.dstack(sample_array_lst)
+    label_array = np.concatenate(label_array_lst)
+    sample_array = sample_array.transpose(2, 0, 1)
+    print ("sample_array.shape", sample_array.shape)
+    print ("label_array.shape", label_array.shape)
+    return sample_array, label_array
+
+
+def load_array (sample_dir_path, unit_num, win_len, stride):
+    filename =  'Unit%s_win%s_str%s.npz' %(str(int(unit_num)), win_len, stride)
+    filepath =  os.path.join(sample_dir_path, filename)
+    loaded = np.load(filepath)
+
+    return loaded['sample'].transpose(2, 0, 1), loaded['label']
+
+def rmse(y_true, y_pred):
+    return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+
+def shuffle_array(sample_array, label_array):
+    ind_list = list(range(len(sample_array)))
+    print("ind_list befor: ", ind_list[:10])
+    print("ind_list befor: ", ind_list[-10:])
+    ind_list = shuffle(ind_list)
+    print("ind_list after: ", ind_list[:10])
+    print("ind_list after: ", ind_list[-10:])
+    print("Shuffeling in progress")
+    shuffle_sample = sample_array[ind_list, :, :]
+    shuffle_label = label_array[ind_list,]
+    return shuffle_sample, shuffle_label
+
+def figsave(history, win_len, win_stride, bs, lr, sub):
+    fig_acc = plt.figure(figsize=(15, 8))
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Training', fontsize=24)
+    plt.ylabel('loss', fontdict={'fontsize': 18})
+    plt.xlabel('epoch', fontdict={'fontsize': 18})
+    plt.legend(['Training loss', 'Validation loss'], loc='upper left', fontsize=18)
+    plt.show()
+    print ("saving file:training loss figure")
+    fig_acc.savefig(pic_dir + "/training_w%s_s%s_bs%s_sub%s_lr%s.png" %(int(win_len), int(win_stride), int(bs), int(sub), str(lr)))
+    return
+
+
+
+def get_flops(model):
+    concrete = tf.function(lambda inputs: model(inputs))
+    concrete_func = concrete.get_concrete_function(
+        [tf.TensorSpec([1, *inputs.shape[1:]]) for inputs in model.inputs])
+    frozen_func, graph_def = convert_variables_to_constants_v2_as_graph(concrete_func)
+    with tf.Graph().as_default() as graph:
+        tf.graph_util.import_graph_def(graph_def, name='')
+        run_meta = tf.compat.v1.RunMetadata()
+        opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+        flops = tf.compat.v1.profiler.profile(graph=graph, run_meta=run_meta, cmd="op", options=opts)
+        return flops.total_float_ops
+
+
+
+
+def scheduler(epoch, lr):
+    if epoch == 30:
+        print("lr decay by 10")
+        return lr * 0.1
+    elif epoch == 70:
+        print("lr decay by 10")
+        return lr * 0.1
+    else:
+        return lr
+
+
+
+def release_list(a):
+   del a[:]
+   del a
+
+units_index_train = [2.0, 5.0, 10.0, 16.0, 18.0, 20.0]
+units_index_test = [11.0, 14.0, 15.0]
+
+
+
+def main():
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    parser = argparse.ArgumentParser(description='sample creator')
+    parser.add_argument('-i', type=int, default=20, help='n_input_nodes')
+    parser.add_argument('-h', type=int, default=100, help='n_hidden_nodes')
+    parser.add_argument('-o', type=int, default=1, help='n_output_nodes')
+
+
+    parser.add_argument('-k', type=int, default=10, help='size of kernel')
+    parser.add_argument('-bs', type=int, default=128, help='batch size')
+    parser.add_argument('-ep', type=int, default=30, help='max epoch')
+    parser.add_argument('-pt', type=int, default=20, help='patience')
+    parser.add_argument('-vs', type=float, default=0.1, help='validation split')
+    parser.add_argument('-lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('-sub', type=int, default=10, help='subsampling stride')
+
+
+    args = parser.parse_args()
+
+    win_len = args.w
+    win_stride = args.s
+    partition = 3
+    n_filters = args.f
+    kernel_size = args.k
+    lr = args.lr
+    bs = args.bs
+    ep = args.ep
+    pt = args.pt
+    vs = args.vs
+    sub = args.sub
+
+    n_input_nodes = args.i
+    n_hidden_nodes = args.h
+    n_output_nodes = args.o
+
+    os_elm = OS_ELM(
+        # the number of input nodes.
+        n_input_nodes=n_input_nodes,
+        # the number of hidden nodes.
+        n_hidden_nodes=n_hidden_nodes,
+        # the number of output nodes.
+        n_output_nodes=n_output_nodes,
+        # loss function.
+        # the default value is 'mean_squared_error'.
+        # for the other functions, we support
+        # 'mean_absolute_error', 'categorical_crossentropy', and 'binary_crossentropy'.
+        loss='mean_squared_error',
+        # activation function applied to the hidden nodes.
+        # the default value is 'sigmoid'.
+        # for the other functions, we support 'linear' and 'tanh'.
+        # NOTE: OS-ELM can apply an activation function only to the hidden nodes.
+        activation='sigmoid',
+    )
+
+
+
+    train_units_samples_lst =[]
+    train_units_labels_lst = []
+
+    for index in units_index_train:
+        print("Load data index: ", index)
+        sample_array, label_array = load_array (sample_dir_path, index, win_len, win_stride)
+        #sample_array, label_array = shuffle_array(sample_array, label_array)
+        print("sample_array.shape", sample_array.shape)
+        print("label_array.shape", label_array.shape)
+        sample_array = sample_array[::sub]
+        label_array = label_array[::sub]
+        print("sub sample_array.shape", sample_array.shape)
+        print("sub label_array.shape", label_array.shape)
+        train_units_samples_lst.append(sample_array)
+        train_units_labels_lst.append(label_array)
+
+    sample_array = np.concatenate(train_units_samples_lst)
+    label_array = np.concatenate(train_units_labels_lst)
+    print ("samples are aggregated")
+
+    release_list(train_units_samples_lst)
+    release_list(train_units_labels_lst)
+    train_units_samples_lst =[]
+    train_units_labels_lst = []
+    print("Memory released")
+
+    sample_array, label_array = shuffle_array(sample_array, label_array)
+    print("samples are shuffled")
+    print("sample_array.shape", sample_array.shape)
+    print("label_array.shape", label_array.shape)
+
+    sample_array = sample_array.reshape(sample_array.shape[0], sample_array.shape[2])
+    print("sample_array_reshape.shape", sample_array.shape)
+    print("label_array_reshape.shape", label_array.shape)
+    feat_len = sample_array.shape[1]
+    print ("feat_len", feat_len)
+    print ("n_input_nodes",  n_input_nodes)
+
+    # divide the training dataset into two datasets:
+    # (1) for the initial training phase
+    # (2) for the sequential training phase
+    # NOTE: the number of training samples for the initial training phase
+    # must be much greater than the number of the model's hidden nodes.
+    # here, we assign int(1.5 * n_hidden_nodes) training samples
+    # for the initial training phase.
+    n_classes = n_output_nodes
+
+    border = int(1.5 * n_hidden_nodes)
+    x_train_init = sample_array[:border]
+    x_train_seq = sample_array[border:]
+    t_train_init = label_array[:border]
+    t_train_seq = label_array[border:]
+
+
+    print ("x_train_init.shape", x_train_init.shape)
+    print("x_train_seq.shape", x_train_seq.shape)
+    print("t_train_init.shape", t_train_init.shape)
+    print("t_train_seq.shape", t_train_seq.shape)
+
+    # ===========================================
+    # Training
+    # ===========================================
+    # the initial training phase
+    pbar = tqdm.tqdm(total=len(sample_array), desc='initial training phase')
+    os_elm.init_train(x_train_init, t_train_init)
+    pbar.update(n=len(x_train_init))
+
+    # the sequential training phase
+    pbar.set_description('sequential training phase')
+    batch_size = bs
+    for i in range(0, len(x_train_seq), batch_size):
+        x_batch = x_train_seq[i:i + batch_size]
+        t_batch = t_train_seq[i:i + batch_size]
+        os_elm.seq_train(x_batch, t_batch)
+        pbar.update(n=len(x_batch))
+    pbar.close()
+
+    # ===========================================
+    # Prediction
+    # ===========================================
+    print('saving model parameters...')
+    os_elm.save('./checkpoint/model.ckpt')
+
+    output_lst = []
+    truth_lst = []
+
+    for index in units_index_test:
+        print ("test idx: ", index)
+        sample_array, label_array = load_array(sample_dir_path, index, win_len, win_stride)
+        # estimator = load_model(tf_temp_path, custom_objects={'rmse':rmse})
+        print("sample_array.shape", sample_array.shape)
+        print("label_array.shape", label_array.shape)
+        sample_array = sample_array[::sub]
+        label_array = label_array[::sub]
+        print("sub sample_array.shape", sample_array.shape)
+        print("sub label_array.shape", label_array.shape)
+        sample_array = sample_array.reshape(sample_array.shape[0], sample_array.shape[2])
+        print("sample_array_reshape.shape", sample_array.shape)
+        print("label_array_reshape.shape", label_array.shape)
+
+        # estimator = load_model(model_temp_path)
+        # y_pred_test = estimator.predict(sample_array)
+
+        y_pred_test = os_elm.predict(sample_array)
+
+
+        output_lst.append(y_pred_test)
+        truth_lst.append(label_array)
+
+    print(output_lst[0].shape)
+    print(truth_lst[0].shape)
+
+    print(np.concatenate(output_lst).shape)
+    print(np.concatenate(truth_lst).shape)
+
+    output_array = np.concatenate(output_lst)[:, 0]
+    trytg_array = np.concatenate(truth_lst)
+    print(output_array.shape)
+    print(trytg_array.shape)
+    rms = sqrt(mean_squared_error(output_array, trytg_array))
+    print(rms)
+    rms = round(rms, 2)
+
+    end = time.time()
+    num_test = output_array.shape[0]
+
+    for idx in range(len(units_index_test)):
+        print(output_lst[idx])
+        print(truth_lst[idx])
+        fig_verify = plt.figure(figsize=(24, 10))
+        plt.plot(output_lst[idx], color="green")
+        plt.plot(truth_lst[idx], color="red", linewidth=2.0)
+        plt.title('Unit%s inference' %str(int(units_index_test[idx])), fontsize=30)
+        plt.yticks(fontsize=20)
+        plt.xticks(fontsize=20)
+        plt.ylabel('RUL', fontdict={'fontsize': 24})
+        plt.xlabel('Timestamps', fontdict={'fontsize': 24})
+        plt.legend(['Predicted', 'Truth'], loc='upper right', fontsize=28)
+        plt.show()
+        fig_verify.savefig(pic_dir + "/elm_unit%s_test_h%s_rmse-%s.png" %(str(int(units_index_test[idx])),
+                                                                              int(n_hidden_nodes), str(rms)))
+
+
+
+    print("Result in RMSE: ", rms)
+
+
+if __name__ == '__main__':
+    main()
